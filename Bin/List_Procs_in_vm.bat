b003f2552473dd9a3f8d46509b5fc3e1f4fead60
mark <mark.zvuluni@incredibuild.com>
2018-01-25 15:53:43 +0200

    Mark Z: Remove OLD REPO files Change Version Control on project to GIT

--- Bin/List_Procs_in_vm.bat
+++
@@ -1,5 +0,0 @@
-rem %1 - Guest user
-rem %2 - Guest Password
-rem %3 - path to the .vmx file of virtual machine (must be running!)
-
-"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" -gu %1 -gp %1  listProcessesInGuest %3
--- Bin/Stop_Remote_VM.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem usage:
-rem %1 - IP of the remote Host machine
-rem %2 - path to the remote .vmx file (note that the path is case sensetive), e.g. 	VM-Sim-ubu1404\Ubuntu_14.vmx
-"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe" -T ws-shared -h https://%1:443/sdk -u administrator -p 4illumination stop  "[ha-datacenter/standard] %2"  
