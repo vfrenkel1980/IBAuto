95135d3d65641d8f4739801325ac93be4952ea84
mark <mark.zvuluni@incredibuild.com>
2018-03-18 16:27:52 +0200

    fix disableVsMonitor

--- src/main/java/frameworkInfra/testbases/VSIntegrationTestBase.java
+++ src/main/java/frameworkInfra/testbases/VSIntegrationTestBase.java
@@ -81,6 +81,7 @@ public class VSIntegrationTestBase extends TestBase {
                 break;
         }
         vsService.openProject(projectPath);
+        extent.flush();
     }
 
     @BeforeMethod
