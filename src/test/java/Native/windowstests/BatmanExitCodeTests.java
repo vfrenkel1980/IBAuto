f1e535c87b3b250e5d9eb878cbb9f55ebeb9ead2
aleksandra <aleksandra.malykhina@incredibuild.com>
2018-06-20 14:09:29 +0300

    Merge branch 'master' of X:\qa_automation with conflicts.

--- src/test/java/Native/windowstests/BatmanExitCodeTests.java
+++ src/test/java/Native/windowstests/BatmanExitCodeTests.java
@@ -17,20 +17,20 @@ public class BatmanExitCodeTests extends BatmanBCTestBase {
 
     @Test(testName = "/exitcodebase (SUCCESS = 0) Test")
     public void exitCodeBaseTest0() {
-        int returnCode = winService.runCommandWaitForFinish(String.format(ProjectsCommands.MISC_PROJECTS.XG_CONSOLE_SAMPLE + " /exitcodebase=10"));
+        int returnCode = winService.runCommandWaitForFinish(ProjectsCommands.MISC_PROJECTS.XG_CONSOLE_SAMPLE + " /exitcodebase=10");
         Assert.assertTrue(returnCode == 0, "exitCodeBaseTest0 failed with return code " + returnCode);
     }
 
     @Test(testName = "/exitcodebase (BUILD_ERROR = 1) Test")
     public void exitCodeBaseTest1() {
-        int returnCode = winService.runCommandWaitForFinish(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.MISC_PROJECTS.FAILEDPROJECT_X64_DEBUG + " /rebuild /exitcodebase"));
+        int returnCode = winService.runCommandWaitForFinish(IbLocations.BUILD_CONSOLE + ProjectsCommands.MISC_PROJECTS.FAILEDPROJECT_X64_DEBUG + " /exitcodebase");
         Assert.assertTrue(returnCode == 1, "exitCodeBaseTest1 failed with return code " + returnCode);
     }
 
     @Test(testName = "/exitcodebase (USER_CANCELED = 2) Test")
     public void exitCodeBaseTest2() {
         String result = "";
-        winService.runCommandDontWaitForTermination(IbLocations.BUILD_CONSOLE + String.format(TestProjects.VC15PROJECT + " /rebuild /cfg=\"release|x64\" /exitcodebase"));
+        winService.runCommandDontWaitForTermination(ProjectsCommands.MISC_PROJECTS.PROJECTVC15_RELEASE_X64 + " /exitcodebase");
         SystemActions.sleep(2);
         try {
             SystemActions.killProcess(Processes.BUILD_CONSOLE);
@@ -44,15 +44,15 @@ public class BatmanExitCodeTests extends BatmanBCTestBase {
 
     @Test(testName = "/exitcodebase (SYSTEM_ERROR = 3) Test")
     public void exitCodeBaseTest3() {
-        int returnCode = winService.runCommandWaitForFinish(String.format(ProjectsCommands.MISC_PROJECTS.IB_CONSOLE_FAILEDBUILD + " /exitcodebase=100"));
+        int returnCode = winService.runCommandWaitForFinish(ProjectsCommands.MISC_PROJECTS.IB_CONSOLE_FAILEDBUILD + " /exitcodebase=100");
         Assert.assertTrue(returnCode == 103, "exitCodeBaseTest3 failed with return code " + returnCode);
     }
 
     @Test(testName = "/exitcodebase (MAX_BUILDS_REACHED = 4) Test")
     public void exitCodeBaseTest4() {
         setBuildServiceRegistry(RegistryKeys.MAX_CONCURRENT_BUILDS, "1");
-        winService.runCommandDontWaitForTermination(IbLocations.BUILD_CONSOLE + String.format(TestProjects.VC10PROJECT + " /rebuild /cfg=\"debug|win32\" "));
-        int returnCode = winService.runCommandWaitForFinish(String.format(IbLocations.BUILD_CONSOLE + String.format(TestProjects.VC8PROJECT + " /rebuild /cfg=\"debug|win32\" /exitcodebase=-8  /nowait")));
+        winService.runCommandDontWaitForTermination(ProjectsCommands.MISC_PROJECTS.PROJECTVC15_RELEASE_X64);
+        int returnCode = winService.runCommandWaitForFinish(ProjectsCommands.MISC_PROJECTS.PROJECTVC10_DEBUG_WIN32);
         Assert.assertTrue(returnCode == -4, "exitCodeBaseTest4 failed with return code " + returnCode);
     }
 
@@ -60,8 +60,8 @@ public class BatmanExitCodeTests extends BatmanBCTestBase {
     public void exitCodeBaseTest5() {
         setBuildServiceRegistry(RegistryKeys.MIN_LOCAL_CORES, "8");
         setBuildServiceRegistry(RegistryKeys.MAX_CONCURRENT_BUILDS, "2");
-        winService.runCommandDontWaitForTermination(IbLocations.BUILD_CONSOLE + String.format(TestProjects.VC14PROJECT + " /rebuild /cfg=\"release|x64\" "));
-        int returnCode = winService.runCommandWaitForFinish(String.format(IbLocations.BUILD_CONSOLE + String.format(TestProjects.VC15PROJECT + " /rebuild /cfg=\"debug|x64\" /exitcodebase=1000000000000  /nowait")));
+        winService.runCommandDontWaitForTermination(ProjectsCommands.MISC_PROJECTS.PROJECTVC10_DEBUG_WIN32);
+        int returnCode = winService.runCommandWaitForFinish(String.format(ProjectsCommands.MISC_PROJECTS.PROJECTVC15_RELEASE_X64 + " /exitcodebase=1000000000000"));
         Assert.assertTrue(returnCode == 10005, "exitCodeBaseTest5 failed with return code " + returnCode);
     }
 
--- src/test/java/Native/windowstests/BatmanMiscProjTests.java
+++ src/test/java/Native/windowstests/BatmanMiscProjTests.java
@@ -3,6 +3,7 @@ package Native.windowstests;
 import com.aventstack.extentreports.Status;
 import frameworkInfra.testbases.BatmanBCTestBase;
 import frameworkInfra.utils.Parser;
+import frameworkInfra.utils.StaticDataProvider.*;
 import frameworkInfra.utils.StaticDataProvider.Locations;
 import frameworkInfra.utils.StaticDataProvider.ProjectsCommands;
 import org.testng.Assert;
@@ -26,4 +27,24 @@ public class BatmanMiscProjTests extends BatmanBCTestBase {
         }
         Assert.assertTrue(Parser.doesFileContainString(Locations.OUTPUT_LOG_FILE, "Agent '"), "No agents were assigned to the build");
     }
+
+    @Test(testName = "IBCustomStep ON Success Test")
+    public void customStepOnSuccessTest() {
+        int returnCode = winService.runCommandWaitForFinish(ProjectsCommands.MISC_PROJECTS.PROJECTVC10_CUSTOMSTEP_SUCCESS);
+        Assert.assertTrue(returnCode == 0, "customStepOnSuccessTest failed with return code " + returnCode);
+
+    }
+
+    @Test(testName = "IBCustomStep ON Failed Test")
+    public void customStepOnFailTest() {
+        int returnCode = winService.runCommandWaitForFinish(ProjectsCommands.MISC_PROJECTS.PROJECTVC15_CUSTOMSTEP_FAIL);
+        Assert.assertTrue(returnCode == 1, "customStepOnFailTest failed with return code " + returnCode);
 }
+
+
+    //IBCustomStep OFF Success Test
+    //IBCustomStep OFF Fail Test
+}
+
+
+
