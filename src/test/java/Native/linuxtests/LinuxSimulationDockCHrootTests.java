249ce7d4a6d45f1e0c48140704e77d84a01a9826
LP-Neta <neta.zouberer@incredibuild.com>
2018-06-28 19:12:55 +0300

    Docker tests changed

--- src/test/java/Native/linuxtests/LinuxSimulationDockCHrootTests.java
+++ src/test/java/Native/linuxtests/LinuxSimulationDockCHrootTests.java
@@ -13,15 +13,17 @@ public class LinuxSimulationDockCHrootTests extends LinuxSimTestBase {
     public void SimdDockerKenrel4Run() {
 
         linuxService.linuxRunSSHCommand("docker rm IB-Test01", ipList.get(2));
-
         linuxService.linuxRunSSHCommand("/opt/incredibuild/bin/ib_docker run -w /disk2/projects/linux-4.3.3 -v /disk2/projects:/disk2/projects --name IB-Test01 5624cdd7a83e  make clean", ipList.get(2));
-
         linuxService.linuxRunSSHCommand("docker rm IB-Test01", ipList.get(2));
 
         int exitCode = linuxService.linuxRunSSHCommand("/opt/incredibuild/bin/ib_docker run -w /disk2/projects/linux-4.3.3 " +
                 "-v /disk2/projects:/disk2/projects --name IB-Test01 5624cdd7a83e ib_console --ib-crash -d1 -c dockKernel make -j32", ipList.get(2));
 
-        Assert.assertEquals(exitCode, 0, "Test " + testName + " failed with Exit code " + exitCode);
+        if (exitCode != 0) {
+            linuxService.linuxRunSSHCommand("docker stop IB-Test01", ipList.get(2));
+            linuxService.linuxRunSSHCommand("docker rm IB-Test01", ipList.get(2));
+            Assert.assertEquals(exitCode, 0, "Test " + testName + " failed with Exit code " + exitCode);
+        }
 
         linuxService.linuxRunSSHCommand("docker rm IB-Test01", ipList.get(2));
 
@@ -34,17 +36,17 @@ public class LinuxSimulationDockCHrootTests extends LinuxSimTestBase {
     @Test(testName = "Sim docker kenrel4 exec", dependsOnMethods = "SimdDockerKenrel4Run")
     public void SimdDckerKenrel4Exec() {
 
-        int exitCode = linuxService.linuxRunSSHCommand("/opt/incredibuild/bin/ib_docker run -td -w /disk2/projects/linux-4.3.3" +
-                " -v /disk2/projects:/disk2/projects --name IB-Test02 5624cdd7a83e", ipList.get(2));
-
-        if (exitCode != 0) {
-            linuxService.linuxRunSSHCommand("docker stop IB-Test02", ipList.get(2));
-            linuxService.linuxRunSSHCommand("rm IB-Test02", ipList.get(2));
-            Assert.assertEquals(exitCode, 0, "Test " + testName + " failed with Exit code " + exitCode);
-        }
+        int exitCode = linuxService.linuxRunSSHCommand("/opt/incredibuild/bin/ib_docker run -td -w /disk2/projects/linux-4.3.3 -v /disk2/projects:/disk2/projects --name IB-Test02 5624cdd7a83e", ipList.get(2));
 
         linuxService.linuxRunSSHCommand("docker exec IB-Test02 make clean", ipList.get(2));
+        linuxService.linuxRunSSHCommand("docker stop IB-Test02", ipList.get(2));
+        linuxService.linuxRunSSHCommand("docker rm IB-Test02", ipList.get(2));
+        Assert.assertEquals(exitCode, 0, "Test " + testName + "a failed with Exit code " + exitCode);
+
+        exitCode = linuxService.linuxRunSSHCommand("/opt/incredibuild/bin/ib_docker run -td -w /disk2/projects/linux-4.3.3 -v /disk2/projects:/disk2/projects --name IB-Test02 5624cdd7a83e", ipList.get(2));
+        Assert.assertEquals(exitCode, 0, "Test " + testName + "a failed with Exit code " + exitCode);
         exitCode = linuxService.linuxRunSSHCommand("docker exec IB-Test02 ib_console --ib-crash -d1 -c exec_dockKernel make -j32", ipList.get(2));
+
         linuxService.linuxRunSSHCommand("docker exec IB-Test02 make clean", ipList.get(2));
         linuxService.linuxRunSSHCommand("docker stop IB-Test02", ipList.get(2));
         linuxService.linuxRunSSHCommand("docker rm IB-Test02", ipList.get(2));
@@ -54,42 +56,44 @@ public class LinuxSimulationDockCHrootTests extends LinuxSimTestBase {
     @Test(testName = "Sim docker kenrel4 run privileged")
     public void SimdDockerKenrel4RunPrivileged() {
 
-        linuxService.linuxRunSSHCommand("docker rm IB-Test01", ipList.get(2));
+        linuxService.linuxRunSSHCommand("docker rm IB-Test03", ipList.get(2));
 
-        linuxService.linuxRunSSHCommand("/opt/incredibuild/bin/ib_docker run -w /disk2/projects/linux-4.3.3 -v /disk2/projects:/disk2/projects --name IB-Test01 5624cdd7a83e make clean", ipList.get(2));
+        linuxService.linuxRunSSHCommand("/opt/incredibuild/bin/ib_docker run -w /disk2/projects/linux-4.3.3 -v /disk2/projects:/disk2/projects --name IB-Test03 5624cdd7a83e make clean", ipList.get(2));
 
-        linuxService.linuxRunSSHCommand("docker rm IB-Test01", ipList.get(2));
+        linuxService.linuxRunSSHCommand("docker rm IB-Test03", ipList.get(2));
 
         int exitCode = linuxService.linuxRunSSHCommand("/opt/incredibuild/bin/ib_docker run -w /disk2/projects/linux-4.3.3 " +
-                "-v /disk2/projects:/disk2/projects --privileged --name IB-Test01 5624cdd7a83e ib_console --ib-crash -d1 -c dockKernelPrivileged make -j32", ipList.get(2));
+                "-v /disk2/projects:/disk2/projects --privileged --name IB-Test03 5624cdd7a83e ib_console --ib-crash -d1 -c dockKernelPrivileged make -j32", ipList.get(2));
 
-        Assert.assertEquals(exitCode, 0, "Test " + testName + " failed with Exit code " + exitCode);
+        if (exitCode != 0) {
+            linuxService.linuxRunSSHCommand("docker stop IB-Test03", ipList.get(2));
+            linuxService.linuxRunSSHCommand("docker rm IB-Test03", ipList.get(2));
+            Assert.assertEquals(exitCode, 0, "Test " + testName + " failed with Exit code " + exitCode);
+        }
 
-        linuxService.linuxRunSSHCommand("docker rm IB-Test01", ipList.get(2));
+        linuxService.linuxRunSSHCommand("docker rm IB-Test03", ipList.get(2));
 
         linuxService.linuxRunSSHCommand("/opt/incredibuild/bin/ib_docker run -w /disk2/projects/linux-4.3.3 " +
-                "-v /disk2/projects:/disk2/projects --name IB-Test01 5624cdd7a83e make clean", ipList.get(2));
+                "-v /disk2/projects:/disk2/projects --name IB-Test03 5624cdd7a83e make clean", ipList.get(2));
 
-        linuxService.linuxRunSSHCommand("docker rm IB-Test01", ipList.get(2));
+        linuxService.linuxRunSSHCommand("docker rm IB-Test03", ipList.get(2));
     }
 
     @Test(testName = "Sim docker kenrel4 exec privileged", dependsOnMethods = "SimdDockerKenrel4Run")
     public void SimdDckerKenrel4ExecPrivileged() {
 
-        int exitCode = linuxService.linuxRunSSHCommand("/opt/incredibuild/bin/ib_docker run -td -w /disk2/projects/linux-4.3.3" +
-                " -v /disk2/projects:/disk2/projects --privileged --name IB-Test02 5624cdd7a83", ipList.get(2));
+        int exitCode = linuxService.linuxRunSSHCommand("/opt/incredibuild/bin/ib_docker run -td -w /disk2/projects/linux-4.3.3 -v /disk2/projects:/disk2/projects --privileged --name IB-Test04 5624cdd7a83", ipList.get(2));
 
-        if (exitCode != 0) {
-            linuxService.linuxRunSSHCommand("docker stop IB-Test02", ipList.get(2));
-            linuxService.linuxRunSSHCommand("rm IB-Test02", ipList.get(2));
-            Assert.assertEquals(exitCode, 0, "Test " + testName + " failed with Exit code " + exitCode);
-        }
+        linuxService.linuxRunSSHCommand("docker exec IB-Test04 make clean", ipList.get(2));
+        linuxService.linuxRunSSHCommand("docker stop IB-Test04", ipList.get(2));
+        linuxService.linuxRunSSHCommand("docker rm IB-Test04", ipList.get(2));
+        Assert.assertEquals(exitCode, 0, "Test " + testName + "a failed with Exit code " + exitCode);
 
-        linuxService.linuxRunSSHCommand("docker exec IB-Test02 make clean", ipList.get(2));
-        exitCode = linuxService.linuxRunSSHCommand("docker exec IB-Test02 ib_console --ib-crash -d1 -c exec_dockKernelPrivileged make -j32", ipList.get(2));
-        linuxService.linuxRunSSHCommand("docker exec IB-Test02 make clean", ipList.get(2));
-        linuxService.linuxRunSSHCommand("docker stop IB-Test02", ipList.get(2));
-        linuxService.linuxRunSSHCommand("docker rm IB-Test02", ipList.get(2));
+        exitCode = linuxService.linuxRunSSHCommand("/opt/incredibuild/bin/ib_docker run -td -w /disk2/projects/linux-4.3.3 -v /disk2/projects:/disk2/projects --privileged --name IB-Test04 5624cdd7a83", ipList.get(2));
+        exitCode = linuxService.linuxRunSSHCommand("docker exec IB-Test04 ib_console --ib-crash -d1 -c exec_dockKernelPrivileged make -j32", ipList.get(2));
+        linuxService.linuxRunSSHCommand("docker exec IB-Test04 make clean", ipList.get(2));
+        linuxService.linuxRunSSHCommand("docker stop IB-Test04", ipList.get(2));
+        linuxService.linuxRunSSHCommand("docker rm IB-Test04", ipList.get(2));
         Assert.assertEquals(exitCode, 0, "Test " + testName + " failed with Exit code " + exitCode);
     }
 
