170f50db44968330e628b8401d40eebb8723f8d9
mark <mark.zvuluni@incredibuild.com>
2018-08-26 14:37:48 +0300

    .

--- src/main/java/ibInfra/vs/VSUIService.java
+++ src/main/java/ibInfra/vs/VSUIService.java
@@ -116,7 +116,7 @@ public class VSUIService implements IVSUIService {
         driver.findElementByName(action).click();
         test.log(Status.INFO, "Successfully clicked on " + action);
         SystemActions.sleep(3);
-        winService.waitForProcessToFinish(Processes.BUILD_CONSOLE);
+        winService.waitForProcessToFinish("buildsystem.exe");
     }
 
     @Override
