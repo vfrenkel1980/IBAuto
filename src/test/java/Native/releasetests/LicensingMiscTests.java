7ed227282df20f069526d439f078935e9fad1e37
aleksandra <aleksandra.malykhina@incredibuild.com>
2018-10-03 18:23:54 +0300

    License Misc Test edited due to the issue with licence warning message

--- src/test/java/Native/releasetests/LicensingMiscTests.java
+++ src/test/java/Native/releasetests/LicensingMiscTests.java
@@ -29,14 +29,13 @@ public class LicensingMiscTests extends LicensingTestBase {
     public void verifyAlocatedPackagesSaved(){
         ibService.loadIbLicense("IncrediBuild - Vlad - License Testing Environment April 2018.IB_lic");
         winService.runCommandWaitForFinish(StaticDataProvider.IbLocations.XGCOORDCONSOLE + "/AllocateAll");
-        int returncode = winService.runCommandWaitForFinish(StaticDataProvider.IbLocations.XGCOORDCONSOLE + "/deallocatePackages=\"VC678 Yearly\"");
+        int returncode = winService.runCommandWaitForFinish(StaticDataProvider.IbLocations.XGCOORDCONSOLE + "/deallocatePackages=\"VC678 Yearly\",\"C# Yearly\"");
         winService.restartService(StaticDataProvider.WindowsServices.COORD_SERVICE);
         int exitStatus = ibService.cleanAndBuild(StaticDataProvider.IbLocations.BUILD_CONSOLE + StaticDataProvider.Locations.LICENSE_TEST_PROJECTS + StaticDataProvider.LicTestPrjBuildConsoleCommands.VS2017_CPP);
         if (returncode + exitStatus == 0) {
             Assert.assertTrue(Parser.doesFileContainString(StaticDataProvider.Locations.OUTPUT_LOG_FILE, "One of the following licenses is required in order to run this build in distributed mode"));
             //   Assert.assertTrue(Parser.doesFileContainString(StaticDataProvider.Locations.OUTPUT_LOG_FILE, "IncrediBuild for Microsoft Visual Studio C/C++"));
             Assert.assertTrue(Parser.doesFileContainString(StaticDataProvider.Locations.OUTPUT_LOG_FILE, "IncrediBuild for C#"));
-            Assert.assertTrue(Parser.doesFileContainString(StaticDataProvider.Locations.OUTPUT_LOG_FILE, "The \"IncrediBuild for Microsoft Visual Studio C/C++\" license is required"));
         } else {
             Assert.assertTrue(false, "Verify Alocated Packages Saved Test failed");
         }
