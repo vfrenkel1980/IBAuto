dadb5bcf4c0ae5a2f2ae72e806379dc153f10fa5
LP-Neta <neta.zouberer@incredibuild.com>
2018-10-07 17:49:38 +0300

    Linux Simulation : chroot fix

--- src/test/java/Native/linuxtests/LinuxSimulationDockCHrootTests.java
+++ src/test/java/Native/linuxtests/LinuxSimulationDockCHrootTests.java
@@ -104,7 +104,7 @@ public class LinuxSimulationDockCHrootTests extends LinuxSimTestBase {
         if (exitCode > 0)
             linuxService.linuxRunSSHCommand("sudo mount  --bind /disk2 /chroot/xenial_u16/disk2; sudo -S true; sudo mount --bind /usr /chroot/xenial_u16/usr; sudo -S true", ipList.get(simClassType.ordinal()));
 
-       exitCode = linuxService.linuxRunSSHCommand("sudo chroot /chroot/xenial_u16 /bin/bash -c \"" + StaticDataProvider.LinuxSimulation.CHROOT_KERNEL4_DIR + "; "
+       exitCode = linuxService.linuxRunSSHCommand("sudo chroot /chroot/xenial_u16 /bin/bash -c \" cd "+ StaticDataProvider.LinuxSimulation.CHROOT_KERNEL4_DIR + "; "
                + StaticDataProvider.LinuxSimulation.MAKE_CLEAN + "; " + String.format(StaticDataProvider.LinuxSimulation.MAKE_BUILD,"--ib-crash -d1 --f","chroot_Kernel4", "", "32") + " ;echo $?\"", ipList.get(simClassType.ordinal()));
 
        Assert.assertTrue(exitCode <= 0, "Test " + testName + "failed with Exit code " + exitCode);
