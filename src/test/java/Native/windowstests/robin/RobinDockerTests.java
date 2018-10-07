b28704c052fd4bffbed44066db6290c5e4c8c3c4
mark <mark.zvuluni@incredibuild.com>
2018-10-07 09:38:40 +0300

    .

--- src/test/java/Native/windowstests/robin/RobinDockerTests.java
+++ src/test/java/Native/windowstests/robin/RobinDockerTests.java
@@ -1,7 +1,7 @@
 package Native.windowstests.robin;
 
 import frameworkInfra.testbases.RobinDockerTestBase;
-import frameworkInfra.utils.StaticDataProvider;
+import frameworkInfra.utils.StaticDataProvider.*;
 import ibInfra.ibService.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,13 +10,13 @@ public class RobinDockerTests extends RobinDockerTestBase {
 
     @Test(testName = "Docker Mono - Debug - Build")
     public void dockerMonoDebugBuild() {
-        int returnCode = ibService.cleanAndBuild("docker exec -it affectionate_swartz " + StaticDataProvider.IbLocations.BUILD_CONSOLE + String.format(StaticDataProvider.ProjectsCommands.DOCKER_ROBIN.MONO_X64_DEBUG, "%s"));
+        int returnCode = ibService.cleanAndBuild("docker exec -it affectionate_swartz " + IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.DOCKER_ROBIN.MONO_X64_DEBUG, "%s"));
         Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
     }
 
     @Test(testName = "Docker Audacity 2017 - Debug - build", groups = {"Build"})
     public void dockerAudacityDebugBuild() {
-        int returnCode = ibService.cleanAndBuild("docker exec -it affectionate_swartz "+StaticDataProvider.IbLocations.BUILD_CONSOLE + String.format(StaticDataProvider.ProjectsCommands.DOCKER_ROBIN.AUDACITY_X32_DEBUG, "%s"));
+        int returnCode = ibService.cleanAndBuild("docker exec -it affectionate_swartz "+IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.DOCKER_ROBIN.AUDACITY_X32_DEBUG, "%s"));
         Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
     }
 }
