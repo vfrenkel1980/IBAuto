9b38194aade69ab53569697e85e9e0a16954ad68
aleksandra <aleksandra.malykhina@incredibuild.com>
2018-10-16 12:06:53 +0300

    AlfredSim is added

---
+++ src/main/java/frameworkInfra/testbases/AlfredTestBase.java
@@ -0,0 +1,29 @@
+package frameworkInfra.testbases;
+
+import com.aventstack.extentreports.Status;
+import frameworkInfra.Listeners.SuiteListener;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
+
+import java.lang.reflect.Method;
+
+import static frameworkInfra.Listeners.SuiteListener.extent;
+import static frameworkInfra.Listeners.SuiteListener.test;
+
+@Listeners(SuiteListener.class)
+public class AlfredTestBase extends WindowsTestBase{
+
+    @BeforeMethod
+    @Parameters({ "logLevel"})
+    public void beforeMethod(Method method, String logLevel){
+        testName = getTestName(method);
+        test = extent.createTest(testName);
+        if (logLevel.equals("4"))
+            test.assignCategory("Robin Cl - Detailed logging");
+        if (logLevel.equals("0"))
+            test.assignCategory("Robin CL - Minimal logging");
+        test.log(Status.INFO, method.getName() + " test started");
+        log.info(method.getName() + " test started");
+    }
+}
---
+++ src/test/java/Native/windowstests/alfred/AlfredTests.java
@@ -0,0 +1,15 @@
+package Native.windowstests.alfred;
+
+import frameworkInfra.testbases.AlfredTestBase;
+import frameworkInfra.utils.StaticDataProvider;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AlfredTests extends AlfredTestBase {
+
+    @Test(testName = "Audacity 2017 - Debug - build" , groups = { "Build" })
+    public void audacityDebugBuild(){
+        int returnCode = ibService.cleanAndBuild(StaticDataProvider.IbLocations.BUILD_CONSOLE + String.format(StaticDataProvider.ProjectsCommands.VC15_ROBIN.AUDACITY_X32_DEBUG, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+}
