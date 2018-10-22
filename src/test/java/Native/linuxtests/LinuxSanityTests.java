1502c187d25b95693d41dc267059f711a629c179
LP-Neta <neta.zouberer@incredibuild.com>
2018-10-22 14:18:30 +0300

    Linux Sanity : added initial vm handling

--- src/test/java/Native/linuxtests/LinuxSanityTests.java
+++ src/test/java/Native/linuxtests/LinuxSanityTests.java
@@ -11,11 +11,13 @@ import org.testng.annotations.Test;
 public class LinuxSanityTests extends LinuxSanityTestBase {
 
     @Test(testName = "Sim Sanity kernel")
-    public void Linux3SanityTests() {
+    public void LinuxSanityTests() {
+
+        int exitCode = linuxService.linuxRunSSHCommand("echo LinuxSanityTests > LinuxSanityTests",SanityHostName);
 //        int exitCode = linuxService.linuxRunSSHCommand(StaticDataProvider.LinuxSimulation.CD_KERNEL4_SANITY_DIR + ";" + StaticDataProvider.LinuxSimulation.MAKE_CLEAN + ";" +
 //                String.format(StaticDataProvider.LinuxSimulation.MAKE_BUILD, "--ib-crash -d1 --f", "32_bit_Kernel", "", "32"), ipList.get(simClassType.ordinal()));
 //        linuxService.linuxRunSSHCommand(StaticDataProvider.LinuxSimulation.CD_32BIT_KERNEL_DIR + ";" + StaticDataProvider.LinuxSimulation.MAKE_CLEAN + ";", ipList.get(simClassType.ordinal()));
-//        Assert.assertTrue((exitCode <= 0), "Sim 32Bit kernel failed with Exit code " + exitCode);
+        Assert.assertTrue((exitCode <= 0), "Sim LinuxSanityTests failed with Exit code " + exitCode);
     }
 }
 
