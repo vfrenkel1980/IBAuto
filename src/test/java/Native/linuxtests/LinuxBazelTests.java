e9f3903e75afa7e19f71c67eb0643a9dbf11d216
LP-Neta <neta.zouberer@incredibuild.com>
2018-10-28 11:37:43 +0200

    Linux Sim - Bazel - no --f

--- src/test/java/Native/linuxtests/LinuxBazelTests.java
+++ src/test/java/Native/linuxtests/LinuxBazelTests.java
@@ -12,7 +12,7 @@ public class LinuxBazelTests extends LinuxSimTestBase {
     public void SimTest32BitKernel() {
 
         int exitCode = linuxService.linuxRunSSHCommand(StaticDataProvider.LinuxSimulation.CD_TENSOR_DIR + ";" + StaticDataProvider.LinuxSimulation.BAZEL_CLEAN + ";" +
-                String.format(StaticDataProvider.LinuxSimulation.BAZEL_BUILD, "--ib-crash -d1 --f", "tensor", "", "24"), ipList.get(simClassType.ordinal()));
+                String.format(StaticDataProvider.LinuxSimulation.BAZEL_BUILD, "--ib-crash -d1", "tensor", "", "24"), ipList.get(simClassType.ordinal()));
         linuxService.linuxRunSSHCommand(StaticDataProvider.LinuxSimulation.CD_TENSOR_DIR + ";" + StaticDataProvider.LinuxSimulation.BAZEL_CLEAN + ";", ipList.get(simClassType.ordinal()));
         Assert.assertTrue((exitCode <= 0), "Sim tensorflow failed with Exit code " + exitCode);
     }
