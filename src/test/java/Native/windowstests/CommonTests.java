4ff771faa6c7a585528a645c4da0ffa53a7a5e6e
aleksandra <aleksandra.malykhina@incredibuild.com>
2018-12-02 15:34:37 +0200

    Log error checks are added to all win simulations

---
+++ src/test/java/Native/windowstests/CommonTests.java
@@ -0,0 +1,29 @@
+package Native.windowstests;
+import com.aventstack.extentreports.Status;
+import frameworkInfra.testbases.WindowsTestBase;
+import frameworkInfra.utils.parsers.Parser;
+import frameworkInfra.utils.StaticDataProvider.*;
+import frameworkInfra.utils.SystemActions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.List;
+
+import static frameworkInfra.Listeners.SuiteListener.test;
+
+public class CommonTests extends WindowsTestBase {
+
+    @Test(testName = "Verify Errors In Logs")
+    public void verifyErrorsInLogs() {
+        int errorCount = 0;
+        List<String> files = SystemActions.getAllFilesInDirectory(IbLocations.IB_ROOT + "\\logs");
+        for (String file : files) {
+            for (String aERROR_LIST : LogOutput.ERROR_LIST) {
+                if(Parser.doesFileContainString(IbLocations.IB_ROOT + "\\logs\\" + file, aERROR_LIST)) {
+                    errorCount++;
+                    test.log(Status.WARNING, aERROR_LIST + " Appears in " + file);
+                }
+            }
+        }
+        Assert.assertFalse(errorCount > 0);
+    }
+}
