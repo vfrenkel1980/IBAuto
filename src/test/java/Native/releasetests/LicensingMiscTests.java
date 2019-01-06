c449f7e31e23e53418d342d06470d8ba685c3a6b
aleksandra <aleksandra.malykhina@incredibuild.com>
2019-01-06 16:31:18 +0200

    License test fix

--- src/test/java/Native/releasetests/LicensingMiscTests.java
+++ src/test/java/Native/releasetests/LicensingMiscTests.java
@@ -32,6 +32,7 @@ public class LicensingMiscTests extends LicensingTestBase {
         int returncode = winService.runCommandWaitForFinish(StaticDataProvider.IbLocations.XGCOORDCONSOLE + "/deallocatePackages=\"VC678 Yearly\"");
         returncode += winService.runCommandWaitForFinish(StaticDataProvider.IbLocations.XGCOORDCONSOLE + "/deallocatePackages=\"C# Yearly\"");
         winService.restartService(StaticDataProvider.WindowsServices.COORD_SERVICE);
+        SystemActions.sleep(5);
         int exitStatus = ibService.cleanAndBuild(StaticDataProvider.IbLocations.BUILD_CONSOLE + StaticDataProvider.Locations.LICENSE_TEST_PROJECTS + StaticDataProvider.LicTestPrjBuildConsoleCommands.VS2017_CPP);
         if (returncode + exitStatus == 0) {
             Assert.assertTrue(Parser.doesFileContainString(StaticDataProvider.Locations.OUTPUT_LOG_FILE, "One of the following licenses is required in order to run this build in distributed mode"));
