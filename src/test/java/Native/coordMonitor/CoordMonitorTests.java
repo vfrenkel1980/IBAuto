3687c6c1810c686635b2eff799576c56ad00a2ba
aleksandra <aleksandra.malykhina@incredibuild.com>
2019-01-31 11:42:36 +0200

    CoordMon suite..

--- src/test/java/Native/coordMonitor/CoordMonitorTests.java
+++ src/test/java/Native/coordMonitor/CoordMonitorTests.java
@@ -52,7 +52,7 @@ public class CoordMonitorTests extends CoordMonitorTestBase {
     @Test(testName = "Start Coordinator Service", dependsOnMethods = {"stopCoordinatorService"})
     public void startCoordinatorService() {
         coordinator.startCoordService();
-        SystemActions.sleep(5);
+        SystemActions.sleep(8);
         Assert.assertTrue(winService.isServiceRunning(WindowsServices.COORD_SERVICE), "Coordinator service is did not start");
     }
 
