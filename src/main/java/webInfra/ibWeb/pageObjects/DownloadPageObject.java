284053df1a08dd1685651395065c6bf9f9d172ad
aleksandra <aleksandra.malykhina@incredibuild.com>
2019-02-11 12:40:12 +0200

    web ib changes

--- src/main/java/webInfra/ibWeb/pageObjects/DownloadPageObject.java
+++ src/main/java/webInfra/ibWeb/pageObjects/DownloadPageObject.java
@@ -104,7 +104,7 @@ public class DownloadPageObject {
     public static final By invalidphone = By.xpath("//*[contains(text(),'The Phone Number field must be at least 6 characters.')]");
     public static final By phonewithchars = By.xpath("//*[contains(text(),'The Phone Number field may only contain numeric characters.')]");
     public static final By phoneempty = By.xpath("//*[contains(text(),'The Phone Number field is required.')]");
-    public static final By terms = By.xpath("//*[contains(text(),'Please confirm our term and conditions.')]");
+    public static final By terms = By.xpath("//*[contains(text(),'You must approve IncrediBuild’s end user license agreement')]");
     public static final By countryempty = By.xpath("//*[contains(text(),'The Country field is required.')]");
     public static final By companyempty = By.xpath("//*[contains(text(),'The Company field is required.')]");
     public static final By companyshort = By.xpath("//*[contains(text(),'The Company field must be at least 2 characters.')]");
