d52e78ae16966f6a9425a2e71f7a2dc0b1c66baa
aleksandra <aleksandra.malykhina@incredibuild.com>
2019-02-14 12:27:41 +0200

    web updated due to Andrey's changes

--- src/test/java/Web/IBWeb/DownloadFormTests.java
+++ src/test/java/Web/IBWeb/DownloadFormTests.java
@@ -18,7 +18,7 @@ public class DownloadFormTests extends DownloadPageTestBase{
 
     @Test(testName = "Linux Registration")
     public void linuxRegistration(){
-        WindowsRegistrationForm rf = new WindowsRegistrationForm("linux", "User", "blah@blah.com", "123123",
+        WindowsRegistrationForm rf = new WindowsRegistrationForm("linux", "User", "blah@blah.com", "1231231231",
                 "Canada", "Alberta","IB","city", "other", "Brain", true, true,
                 false,false, true, true, true, false, true,false,true,true,
                 true,true,false,true,false,true,true,true,false,true,
@@ -28,7 +28,7 @@ public class DownloadFormTests extends DownloadPageTestBase{
 
     @Test(testName = "Enterprise Registration")
     public void enterpriseRegistration(){
-        WindowsRegistrationForm rf = new WindowsRegistrationForm("enterprise", "User", mailAddress2, "123123",
+        WindowsRegistrationForm rf = new WindowsRegistrationForm("enterprise", "User", mailAddress2, "1231231231",
                 "Israel", "","IB","city", "other", "ballz", true, true,false,
                 false, false, false, false, true, true,true,true,true);
         downloadPageObject.registerEnterpriseUser(rf);
@@ -99,7 +99,7 @@ public class DownloadFormTests extends DownloadPageTestBase{
     @Test(testName = "Windows Registration", dependsOnMethods = { "validateMailing"} )
     public void windowsRegistration(){
         WindowsRegistrationForm rf = new WindowsRegistrationForm("Win", "User", mailAddressRandom, "4illumination",
-                "555954","USA", "alaska", "IB", "MOHA", "other",
+                "5559540098","USA", "alaska", "IB", "MOHA", "other",
                 "KING", false, true, false, true, true, false, false, true, false);
         downloadPageObject.createNewFreeDevWinAccount(rf);
         Assert.assertTrue(GetIsMailRegistered.isMailRegistered(mailAddressRandom));
