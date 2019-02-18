0754b8bbc9c7b118c67669197f24c76e8eb78501
aleksandra <aleksandra.malykhina@incredibuild.com>
2019-02-18 11:43:49 +0200

    unit test

--- src/test/java/Native/UnitTests/UnitTests.java
+++ src/test/java/Native/UnitTests/UnitTests.java
@@ -1,5 +1,6 @@
 package Native.UnitTests;
 
+import frameworkInfra.testbases.BatmanBCTestBase;
 import frameworkInfra.utils.*;
 
 import ibInfra.ibService.IbService;
@@ -11,15 +12,13 @@ import org.testng.annotations.Test;
 import static com.sun.jna.platform.win32.WinReg.*;
 
 
-public class UnitTests {
+public class UnitTests extends BatmanBCTestBase {
     WindowsService winService = new WindowsService();
     IbService ibService = new IbService();
 
     @Test(testName = "test1")
     public void testAttachment() {
         RegistryService.deleteRegKey(HKEY_CLASSES_ROOT,"WOW6432Node\\Interface","{8CA4C95D-CBE4-474A-AB9E-3F8C9313D740}");
-
-
     }
 }
 
