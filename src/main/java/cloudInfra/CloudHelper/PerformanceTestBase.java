be72718686c5ee0554475da2e7e3ebd1772dd85a
mark <mark.zvuluni@incredibuild.com>
2019-04-07 09:52:01 +0300

    add beforesuite for performancetestbase to override cloudtestbase

--- src/main/java/cloudInfra/CloudHelper/PerformanceTestBase.java
+++ src/main/java/cloudInfra/CloudHelper/PerformanceTestBase.java
@@ -1,5 +1,7 @@
 package cloudInfra.CloudHelper;
 
+import cloudInfra.CloudServices.AwsService;
+import cloudInfra.CloudServices.AzureService;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
@@ -9,6 +11,7 @@ import ibInfra.windowscl.WindowsService;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
@@ -30,6 +33,12 @@ public class PerformanceTestBase extends CloudTestBase{
 
     protected WindowsService winService = new WindowsService();
 
+    @BeforeSuite
+    public void init(){
+        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/src/main/java/frameworkInfra/reports/Performance" + formatter.format(calendar.getTime()) + ".html");
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+    }
 
     @AfterMethod
     public void afterMethod(){
