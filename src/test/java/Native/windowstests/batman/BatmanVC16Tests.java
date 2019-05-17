ff2c9463502e70593370f647f64e243f5db20e04
aleksandra <aleksandra.malykhina@incredibuild.com>
2019-05-17 15:27:47 +0300

    VS2019 tests added to batman sim

---
+++ src/test/java/Native/windowstests/batman/BatmanVC16Tests.java
@@ -0,0 +1,43 @@
+package Native.windowstests.batman;
+
+import frameworkInfra.testbases.BatmanBCTestBase;
+import frameworkInfra.utils.parsers.Parser;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static frameworkInfra.utils.StaticDataProvider.*;
+
+public class BatmanVC16Tests extends BatmanBCTestBase {
+
+
+    @Test(testName = "Audacity 2019 - Debug - build", groups = {"Build"})
+    public void audacityDebugBuild() {
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC16_BATMAN.AUDACITY_X32_DEBUG, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "ACE 2019 - Debug - build", groups = {"Build"})
+    public void ace2017DebugBuild() {
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC16_BATMAN.ACE_X32_DEBUG, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "Big Project 2019 - Debug - build", groups = {"Build"})
+    public void bigProjectDebugBuild() {
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC16_BATMAN.BIGPROJECT_X32_DEBUG, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "Big Project 2019 - Release - build", groups = {"Build"})
+    public void bigProjectReleaseBuild() {
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC16_BATMAN.BIGPROJECT_X32_RELEASE, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "Check that \"Predicted\" execution is enable: VS2019 Release")
+    public void checkForPredictedExecutionWithoutMSBuild() {
+        ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC16_BATMAN.AUDACITY_X32_DEBUG, "%s"));
+        Assert.assertFalse(Parser.doesFileContainString(Locations.OUTPUT_LOG_FILE, LogOutput.PREDICTED_DISABLED));
+    }
+
+}
