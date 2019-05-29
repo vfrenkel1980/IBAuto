647a8018242d8c5516c8d871e3212669d5feda0f
mark <mark.zvuluni@incredibuild.com>
2019-05-29 11:46:35 +0300

    IncrediCloud: add uat/prod selection in onboarding tests (add new password for uat)

--- src/main/java/cloudInfra/IncrediCloud/pageObjects/AzureRegistrationPageObject.java
+++ src/main/java/cloudInfra/IncrediCloud/pageObjects/AzureRegistrationPageObject.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static frameworkInfra.testbases.incrediCloud.ICEngineTestBase.ENV;
+
 public class AzureRegistrationPageObject {
 
     //MAPPING
@@ -25,7 +27,14 @@ public class AzureRegistrationPageObject {
     public void selectAzureUser(String user){
         eventWebDriver.findElement(AZURE_EMAIL_TB).sendKeys(user);
         wait.until(ExpectedConditions.elementToBeClickable(NEXT_BUTTON)).click();
-        eventWebDriver.findElement(PASSWORD_TB).sendKeys("4illumination!");
+        switch (ENV){
+            case "prod":
+                eventWebDriver.findElement(PASSWORD_TB).sendKeys("4illumination!");
+                break;
+            case "uat":
+                eventWebDriver.findElement(PASSWORD_TB).sendKeys("4illumination@");
+                break;
+        }
         wait.until(ExpectedConditions.elementToBeClickable(SIGNIN_BUTTON)).click();
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_BUTTON)).click();
     }
