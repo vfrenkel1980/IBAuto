2e7bcfdd9afc82a8e490d5233e3828cb91c7871d
Ronit <ronit.hamer@incredibuild.com>
2019-07-25 18:03:28 +0300

    LinuxSimTestBase - updated CRASH file handling

--- src/main/java/frameworkInfra/testbases/LinuxSimTestBase.java
+++ src/main/java/frameworkInfra/testbases/LinuxSimTestBase.java
@@ -171,9 +171,11 @@ public class LinuxSimTestBase extends LinuxTestBase {
         for (String machine : ipList){
             List<String> has_crashes = linuxService.findFile(machine, "/opt/incredibuild/log/", "*.has_crash");
             for (String crash : has_crashes){
-                if (!crash.isEmpty()){
-                    test.log(Status.WARNING, "Found CRASH file! Machine: " + machine + " Path: " + crash);
+                if (!has_crashes.isEmpty()){
+                    test.log(Status.INFO, "No CRASH file found in Machine: " + machine);
                 }
+                else
+                    test.log(Status.WARNING, "Found CRASH file! Machine: " + machine + " Path: " + crash);
             }
             List<String> log_crashes = linuxService.findFile(machine, "/opt/incredibuild/log/", "*.crash.log");
             for (String crash : log_crashes){
