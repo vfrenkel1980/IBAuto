faab49008658d028320cbc570fb377ebaf933378
aleksandra <aleksandra.malykhina@incredibuild.com>
2019-08-07 09:37:24 +0300

    sleep added to verifyMultiInitiatorAssignment

--- src/test/java/Native/windowstests/vmsim/VmSimVC10Tests.java
+++ src/test/java/Native/windowstests/vmsim/VmSimVC10Tests.java
@@ -76,7 +76,9 @@ public class VmSimVC10Tests extends VmSimTestBase {
     }
 
     /**
-     * @bug <A HREF="http://redmine.incredibuild.local/issues/10834"> Build fails on the windows 8.1 32-bit vm10835</A> Remove minwinver option after fix.
+     * @bug{
+     * Verify <a href="http://redmine.incredibuild.local/issues/10834">uild fails on the windows 8.1 32-bit vm10835</a> Remove minwinver option after fix.
+     * }
      */
     @Test(testName = "All In One Diagnostics Samples 2010 - build" , groups = { "Build" })
     public void allInOne2010x32Debug(){
