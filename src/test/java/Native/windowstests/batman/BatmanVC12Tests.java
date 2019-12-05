8bfb929fd3870c9b21ede7b3c1d48c5468940a93
Yael.Benamu <yael.benamu@incredibuild.com>
2019-12-05 10:34:33 +0200

    Revert "Merge branch 'master' of X:\qa_automation with conflicts."
    This reverts commit 08c15208

--- src/test/java/Native/windowstests/batman/BatmanVC12Tests.java
+++ src/test/java/Native/windowstests/batman/BatmanVC12Tests.java
@@ -10,101 +10,101 @@ import static frameworkInfra.utils.StaticDataProvider.*;
 
 public class BatmanVC12Tests extends BatmanBCTestBase {
 
-//    @Test(testName = "ACE 2013 - Debug - build", groups = {"Build"})
-//    public void ace2013DebugBuild() {
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.ACE_X32_DEBUG, "%s"));
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "ACE 2013 - Release - build", groups = {"Build"})
-//    public void ace2013ReleaseBuild() {
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.ACE_X32_RELEASE, "%s"));
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "Blender 2013 - Debug - build", groups = {"Build"})
-//    public void blender2013DebugBuild() {
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.BLENDER_X32_DEBUG, "%s"));
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "Blender 2013 - Release - build", groups = {"Build"})
-//    public void blender2013ReleaseBuild() {
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.BLENDER_X32_RELEASE, "%s"));
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "OpenCOLLADA 2013 - Debug - build", groups = {"Build"})
-//    public void openCollada2013DebugBuild() {
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.OPENCOLLADA_X32_DEBUG, "%s"));
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "OpenCOLLADA 2013 - Release - build", groups = {"Build"})
-//    public void openCollada2013ReleaseBuild() {
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.OPENCOLLADA_X32_RELEASE, "%s"));
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "LLVM 2013 - Debug - build", groups = {"Build"})
-//    public void llvm2013DebugBuild() {
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.LLVM_X32_DEBUG, "%s"));
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "LLVM 2013 - Release - build", groups = {"Build"})
-//    public void llvm2013ReleaseBuild() {
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.LLVM_X32_RELEASE, "%s"));
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "AccountApplicationAuthorization - 2013 debug|NX32 - build", groups = {"Build"})
-//    public void accountApplicationAuthorization2013DebugNX32Build() {
-//        setRegistry("0", RegistryKeys.PREDICTED);
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.NINTENDO_AAA_NX32_DEBUG, "%s"));
-//        setRegistry("2", RegistryKeys.PREDICTED);
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "AccountApplicationAuthorization - 2013 debug|NX64 - build", groups = {"Build"})
-//    public void accountApplicationAuthorization2013DebugNX64Build() {
-//        setRegistry("0", RegistryKeys.PREDICTED);
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.NINTENDO_AAA_NX64_DEBUG, "%s"));
-//        setRegistry("2", RegistryKeys.PREDICTED);
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "AccountApplicationAuthorization - 2013 release|NX32 - build", groups = {"Build"})
-//    public void accountApplicationAuthorization2013ReleaseNX32Build() {
-//        setRegistry("0", RegistryKeys.PREDICTED);
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.NINTENDO_AAA_NX32_RELEASE, "%s"));
-//        setRegistry("2", RegistryKeys.PREDICTED);
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "AccountApplicationAuthorization - 2013 release|NX64 - build", groups = {"Build"})
-//    public void accountApplicationAuthorization2013ReleaseNX64Build() {
-//        setRegistry("0", RegistryKeys.PREDICTED);
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.NINTENDO_AAA_NX64_RELEASE, "%s"));
-//        setRegistry("2", RegistryKeys.PREDICTED);
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "AccountApplicationAuthorization - 2013 release|x64 - build", groups = {"Build"})
-//    public void accountApplicationAuthorization2013ReleaseX64Build() {
-//        setRegistry("0", RegistryKeys.PREDICTED);
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.NINTENDO_AAA_X64_RELEASE, "%s"));
-//        setRegistry("2", RegistryKeys.PREDICTED);
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
-//
-//    @Test(testName = "AccountApplicationAuthorization - 2013 debug|win32 - build", groups = {"Build"})
-//    public void accountApplicationAuthorization2013DebugX32Build() {
-//        setRegistry("0", RegistryKeys.PREDICTED);
-//        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.NINTENDO_AAA_X32_DEBUG, "%s"));
-//        setRegistry("2", RegistryKeys.PREDICTED);
-//        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
-//    }
+    @Test(testName = "ACE 2013 - Debug - build", groups = {"Build"})
+    public void ace2013DebugBuild() {
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.ACE_X32_DEBUG, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "ACE 2013 - Release - build", groups = {"Build"})
+    public void ace2013ReleaseBuild() {
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.ACE_X32_RELEASE, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "Blender 2013 - Debug - build", groups = {"Build"})
+    public void blender2013DebugBuild() {
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.BLENDER_X32_DEBUG, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "Blender 2013 - Release - build", groups = {"Build"})
+    public void blender2013ReleaseBuild() {
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.BLENDER_X32_RELEASE, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "OpenCOLLADA 2013 - Debug - build", groups = {"Build"})
+    public void openCollada2013DebugBuild() {
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.OPENCOLLADA_X32_DEBUG, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "OpenCOLLADA 2013 - Release - build", groups = {"Build"})
+    public void openCollada2013ReleaseBuild() {
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.OPENCOLLADA_X32_RELEASE, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "LLVM 2013 - Debug - build", groups = {"Build"})
+    public void llvm2013DebugBuild() {
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.LLVM_X32_DEBUG, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "LLVM 2013 - Release - build", groups = {"Build"})
+    public void llvm2013ReleaseBuild() {
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.LLVM_X32_RELEASE, "%s"));
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "AccountApplicationAuthorization - 2013 debug|NX32 - build", groups = {"Build"})
+    public void accountApplicationAuthorization2013DebugNX32Build() {
+        setRegistry("0", RegistryKeys.PREDICTED);
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.NINTENDO_AAA_NX32_DEBUG, "%s"));
+        setRegistry("2", RegistryKeys.PREDICTED);
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "AccountApplicationAuthorization - 2013 debug|NX64 - build", groups = {"Build"})
+    public void accountApplicationAuthorization2013DebugNX64Build() {
+        setRegistry("0", RegistryKeys.PREDICTED);
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.NINTENDO_AAA_NX64_DEBUG, "%s"));
+        setRegistry("2", RegistryKeys.PREDICTED);
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "AccountApplicationAuthorization - 2013 release|NX32 - build", groups = {"Build"})
+    public void accountApplicationAuthorization2013ReleaseNX32Build() {
+        setRegistry("0", RegistryKeys.PREDICTED);
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.NINTENDO_AAA_NX32_RELEASE, "%s"));
+        setRegistry("2", RegistryKeys.PREDICTED);
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "AccountApplicationAuthorization - 2013 release|NX64 - build", groups = {"Build"})
+    public void accountApplicationAuthorization2013ReleaseNX64Build() {
+        setRegistry("0", RegistryKeys.PREDICTED);
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.NINTENDO_AAA_NX64_RELEASE, "%s"));
+        setRegistry("2", RegistryKeys.PREDICTED);
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "AccountApplicationAuthorization - 2013 release|x64 - build", groups = {"Build"})
+    public void accountApplicationAuthorization2013ReleaseX64Build() {
+        setRegistry("0", RegistryKeys.PREDICTED);
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.NINTENDO_AAA_X64_RELEASE, "%s"));
+        setRegistry("2", RegistryKeys.PREDICTED);
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
+
+    @Test(testName = "AccountApplicationAuthorization - 2013 debug|win32 - build", groups = {"Build"})
+    public void accountApplicationAuthorization2013DebugX32Build() {
+        setRegistry("0", RegistryKeys.PREDICTED);
+        int returnCode = ibService.cleanAndBuild(IbLocations.BUILD_CONSOLE + String.format(ProjectsCommands.VC12_BATMAN.NINTENDO_AAA_X32_DEBUG, "%s"));
+        setRegistry("2", RegistryKeys.PREDICTED);
+        Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
+    }
 
     @Test(testName = "NvnTutorial06 - 2013 debug|NX32- build", groups = {"Build"})
     public void nvnTutorial2013DebugNX32Build() {
