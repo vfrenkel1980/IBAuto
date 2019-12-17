06be01343fdf08f78cb5f70a83cb1da6f3367719
aleksandra <aleksandra.malykhina@incredibuild.com>
2019-12-17 13:26:17 +0200

    unitTesting report fix

--- src/main/java/frameworkInfra/testbases/UnitTestingTestBase.java
+++ src/main/java/frameworkInfra/testbases/UnitTestingTestBase.java
@@ -1,23 +1,39 @@
 package frameworkInfra.testbases;
 
+import com.aventstack.extentreports.Status;
 import frameworkInfra.Listeners.SuiteListener;
 import frameworkInfra.utils.RegistryService;
 import frameworkInfra.utils.StaticDataProvider.*;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 
 import java.io.File;
+import java.lang.reflect.Method;
 
 import static com.sun.jna.platform.win32.WinReg.HKEY_LOCAL_MACHINE;
+import static frameworkInfra.Listeners.SuiteListener.extent;
+import static frameworkInfra.Listeners.SuiteListener.test;
 
 @Listeners(SuiteListener.class)
-public class UnitTestingTestBase extends RobinTestBase{
+public class UnitTestingTestBase extends WindowsTestBase{
 
     @BeforeClass
     public void avoidLocal() {
         RegistryService.setRegistryKey(HKEY_LOCAL_MACHINE, Locations.IB_REG_ROOT + "\\Builder", RegistryKeys.AVOID_LOCAL, "1");
+    }
+
+    @BeforeMethod
+    @Parameters({ "logLevel"})
+    public void beforeMethod(Method method, String logLevel, ITestContext testContext){
+        testName = getTestName(method);
+        test = extent.createTest(testName);
+        if (logLevel.equals("4"))
+            test.assignCategory("Unit Testing Cl - Detailed logging");
+        if (logLevel.equals("0"))
+            test.assignCategory("Unit Testing  CL - Minimal logging");
+        test.log(Status.INFO, method.getName() + " test started");
+        log.info(method.getName() + " test started");
+        testName = testContext.getName();
     }
 
     @AfterClass
