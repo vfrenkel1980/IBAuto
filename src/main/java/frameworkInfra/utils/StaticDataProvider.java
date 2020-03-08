978739f0b93538abd7a3231fb82188251292419d
Yael.Benamu <yael.benamu@incredibuild.com>
2020-03-08 16:31:49 +0200

    StatiDataProvider class: missing final COORD_SETTINGS_LOCATION

--- src/main/java/frameworkInfra/utils/StaticDataProvider.java
+++ src/main/java/frameworkInfra/utils/StaticDataProvider.java
@@ -257,6 +257,11 @@ public class StaticDataProvider {
             public static final String LITTLE_PROJECT_X86_DEBUG = "C:\\QA\\Simulation\\projects\\LittleProject2\\LittleProject2.sln /%s /cfg=\"Debug|x86\" /title=\"LittleProject 2017 - Debug\" /VSInstallDir=\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Professional\"";
         }
 
+        //TODO: ADD CORRECT PATH TO COORDSETTINGS
+        public static class COORD_SETTINGS{
+            public static final String COORD_SETTINGS_LOCATION = "C:\\Program Files (x86)\\IncrediBuild\\CoordinatorSettings.exe";
+        }
+
         public static class VC16_BATMAN {
             public static final String AUDACITY_X32_DEBUG = "\"D:\\QA\\Simulation\\VC16\\Audacity\\Audacity 2.1.0 src\\win\\audacity.sln\" /%s /cfg=\"debug|win32\" /title=\"Audacity 2019 - Debug\" /VSInstallDir=\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Professional\" /VSversion=16";
             public static final String ACE_X32_DEBUG = "\"D:\\QA\\Simulation\\VC16\\ACE-6.4.0-2015\\ACE_vc14.sln\" /%s /cfg=\"debug|win32\" /title=\"ACE 2019 - Debug\" /VSInstallDir=\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Professional\" /VSversion=16";
