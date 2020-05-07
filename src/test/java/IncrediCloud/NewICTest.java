fea17cda2e6746c3aaf06946782a3b548ec9facf
Yael <yael.benamu@incredibuild.com>
2020-05-07 11:21:10 +0300

    NewICTest

---
+++ src/test/java/IncrediCloud/NewICTest.java
@@ -0,0 +1,13 @@
+package IncrediCloud;
+
+public class NewICTest {
+    private int i;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
