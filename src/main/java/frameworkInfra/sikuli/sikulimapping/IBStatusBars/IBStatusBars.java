0468f4fafc46a9188c66dd250aeed0816a8f4116
TanyaGaus <tanya.gaus@incredibuild.com>
2020-05-19 11:29:51 +0300

    class parameters changed

--- src/main/java/frameworkInfra/sikuli/sikulimapping/IBStatusBars/IBStatusBars.java
+++ src/main/java/frameworkInfra/sikuli/sikulimapping/IBStatusBars/IBStatusBars.java
@@ -4,8 +4,8 @@ import org.sikuli.script.Pattern;
 
 public class IBStatusBars {
 
-    public static final Pattern buildMonitor = new Pattern("src\\main\\java\\frameworkInfra\\sikuli\\testscreenshots\\IBStatusBars\\build_monitor_status_bar.png");
-    public static final Pattern buildHistory = new Pattern("src\\main\\java\\frameworkInfra\\sikuli\\testscreenshots\\IBStatusBars\\build_history_status_bar.png");
-    public static final Pattern coordinatorMonitor = new Pattern("src\\main\\java\\frameworkInfra\\sikuli\\testscreenshots\\IBStatusBars\\coordinator_status_bar.png");
-    public static final Pattern agentSettings = new Pattern("src\\main\\java\\frameworkInfra\\sikuli\\testscreenshots\\IBStatusBars\\agent_settings_status_bar.png");
+    public static final Pattern buildMonitor = new Pattern(System.getProperty("user.dir") + "\\src\\main\\java\\frameworkInfra\\sikuli\\testscreenshots\\IBStatusBars\\build_monitor_status_bar.png");
+    public static final Pattern buildHistory = new Pattern(System.getProperty("user.dir") + "\\src\\main\\java\\frameworkInfra\\sikuli\\testscreenshots\\IBStatusBars\\build_history_status_bar.png");
+    public static final Pattern coordinatorMonitor = new Pattern(System.getProperty("user.dir") + "\\src\\main\\java\\frameworkInfra\\sikuli\\testscreenshots\\IBStatusBars\\coordinator_status_bar.png");
+    public static final Pattern agentSettings = new Pattern(System.getProperty("user.dir") + "\\src\\main\\java\\frameworkInfra\\sikuli\\testscreenshots\\IBStatusBars\\agent_settings_status_bar.png");
 }
