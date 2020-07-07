1ef7a1d9e92e5f6edc5fcea242f14fb34131c1ac
Yael.Benamu <yael.benamu@incredibuild.com>
2020-07-07 12:18:35 +0300

    Host: Robin Modified classes: RobinVC15 Test: chromeReleaseBuild Action: undo ignore

--- src/test/java/Native/windowstests/robin/RobinVC15Tests.java
+++ src/test/java/Native/windowstests/robin/RobinVC15Tests.java
@@ -11,10 +11,9 @@ import org.testng.annotations.Test;
 import static frameworkInfra.Listeners.SuiteListener.test;
 
 public class RobinVC15Tests extends RobinTestBase {
-    @Ignore
-    @Test(testName = "Chrome release - build" , groups = { "Build" })
-    public void chromeReleaseBuild(){
-        if (testName.equals("Minimal")){
+    @Test(testName = "Chrome release - build", groups = {"Build"})
+    public void chromeReleaseBuild() {
+        if (testName.equals("Minimal")) {
             test.log(Status.SKIP, "Skipping Chrome test on Minimal logging");
             throw new SkipException("Skipped test");
         }
@@ -23,20 +22,20 @@ public class RobinVC15Tests extends RobinTestBase {
         Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
     }
 
-    @Test(testName = "Audacity 2017 - Debug - build" , groups = { "Build" })
-    public void audacity2017DebugBuild(){
+    @Test(testName = "Audacity 2017 - Debug - build", groups = {"Build"})
+    public void audacity2017DebugBuild() {
         int returnCode = ibService.cleanAndBuild(StaticDataProvider.IbLocations.BUILD_CONSOLE + String.format(StaticDataProvider.ProjectsCommands.VC15_ROBIN.AUDACITY_X32_DEBUG, "%s"));
         Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
     }
 
-    @Test(testName = "Blender 2017 - Release - build" , groups = { "Build" })
-    public void blender2017ReleaseBuild(){
+    @Test(testName = "Blender 2017 - Release - build", groups = {"Build"})
+    public void blender2017ReleaseBuild() {
         int returnCode = ibService.cleanAndBuild(StaticDataProvider.IbLocations.BUILD_CONSOLE + String.format(StaticDataProvider.ProjectsCommands.VC15_ROBIN.BLENDER_X64_RELEASE, "%s"));
         Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
     }
 
-    @Test(testName = "ACE 2017 - Debug - build" , groups = { "Build" })
-    public void ace2017DebugBuild(){
+    @Test(testName = "ACE 2017 - Debug - build", groups = {"Build"})
+    public void ace2017DebugBuild() {
         int returnCode = ibService.cleanAndBuild(StaticDataProvider.IbLocations.BUILD_CONSOLE + String.format(StaticDataProvider.ProjectsCommands.VC15_ROBIN.ACE_X32_DEBUG, "%s"));
         Assert.assertTrue(returnCode == 0 || returnCode == 2, "Build failed with return code " + returnCode);
     }
